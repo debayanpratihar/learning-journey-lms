9d8db44d504875a39b3a299b9655c0372aa5821e
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-10-02 22:59:32 +0530

    last add

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -47,7 +47,7 @@ export const columns: ColumnDef<Course>[] = [
       const price = parseFloat(row.getValue("price") || "0");
       const formatted = new Intl.NumberFormat("en-US", {
         style: "currency",
-        currency: "USD"
+        currency: "INR"
       }).format(price);
 
       return <div>{formatted}</div>
