2ac37bedbb277a9feb0d3c8a47b23e4a6bc99cfd
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-10-04 20:01:38 +0530

    Add the INR sign to the chart

--- app/(dashboard)/(routes)/teacher/analytics/_components/chart.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/_components/chart.tsx
@@ -36,7 +36,7 @@ export const Chart = ({
             fontSize={12}
             tickLine={false}
             axisLine={false}
-            tickFormatter={(value) => `$${value}`}
+            tickFormatter={(value) => `₹${value}`}
           />
           <Bar
             dataKey="total"
