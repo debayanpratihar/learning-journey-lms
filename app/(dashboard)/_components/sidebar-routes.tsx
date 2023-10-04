575cc11e7cdba50d63cd03e30da47b335babe21d
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2023-10-04 15:19:43 +0530

    Adding the quizmify AI

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { BarChart, Compass, Layout, List } from "lucide-react";
+import { BrainCircuit , BarChart, Compass, Layout, List } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 import { SidebarItem } from "./sidebar-item";
@@ -15,6 +15,11 @@ const guestRoutes = [
     icon: Compass,
     label: "Browse",
     href: "/search",
+  },
+  {
+    icon: BrainCircuit,
+    label: "Quizmify AI",
+    href: "http://localhost:3000",
   },
 ];
 
